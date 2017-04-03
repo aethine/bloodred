8244c8941719c37c9a80a79f0dbc623b25f568d6
Kaleb Shane <yepicness@gmail.com>
2017-04-03 15:00:33 -0400

    First Version

---
+++ bloodred/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloodred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Bloodred Account Manager";
+        }
+    }
+}
